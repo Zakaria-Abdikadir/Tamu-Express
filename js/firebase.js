002014d5ca7573839d6c23770a1f01676bdbbab4
Zakaria Abdikadir <sakitaakooy114@gmail.com>
2025-10-22 18:16:07 +0300

    Added Firebase integration

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVfsiuuNkV2rDiHpEpbbyZeqR3CKxYnSc",
+  authDomain: "tamu-express.firebaseapp.com",
+  projectId: "tamu-express",
+  storageBucket: "tamu-express.firebasestorage.app",
+  messagingSenderId: "824065764709",
+  appId: "1:824065764709:web:ff26ba041f1bd2f205858c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
